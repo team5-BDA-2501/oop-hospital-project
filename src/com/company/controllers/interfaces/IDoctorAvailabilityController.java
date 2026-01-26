4574c2db448ce45e87636960f2ada1d720efb047
dilnazkhazadiyaz <khzdzdilnaz@gmail.com>
2026-01-26 05:00:12 +0500

    forgot }

--- src/com/company/controllers/interfaces/IDoctorAvailabilityController.java
+++ src/com/company/controllers/interfaces/IDoctorAvailabilityController.java
@@ -5,5 +5,6 @@ import java.util.List;
 
 public interface IDoctorAvailabilityController {
     List<DoctorAvailability> getDoctorAvailability(int doctorId);
+
     boolean addDoctorAvailability(int doctorId, String dayOfWeek, String startTime, String endTime);
 }
